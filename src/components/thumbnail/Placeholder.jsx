793397165ae10700bb17b18244474a7d83fe06e3
Tom Clark <thc29@MacBook-Pro-2.local>
2018-08-09 10:00:38 +0100

    Refactored Placeholder

--- src/components/thumbnail/Placeholder.jsx
+++ src/components/thumbnail/Placeholder.jsx
@@ -1,26 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// TODO remove size and status now they're not used for inline styles
 const Placeholder = ({ className, image, size, status }) => {
-    const style = {
-        display: 'inline-block',
-        maxHeight: size,
-        maxWidth: size
-    }
-
-    return (
-        <div className={ `react-fine-uploader-thumbnail-placeholder react-fine-uploader-thumbnail-${status} ${className || ''}` }
-             style={ style }
-        >
-            { image }
-        </div>
-    )
+  return (
+    <div className={`${className || ''}`}>
+      { image }
+    </div>
+  )
 }
 
 Placeholder.propTypes = {
-    image: PropTypes.node.isRequired,
-    size: PropTypes.number.isRequired,
-    status: PropTypes.string.isRequired
+  image: PropTypes.node.isRequired,
+  size: PropTypes.number.isRequired,
+  status: PropTypes.string.isRequired,
 }
 
 export default Placeholder
