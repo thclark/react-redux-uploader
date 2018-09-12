1b669c53570ebab1b6d098121d406e9c8ce5919e
Tom Clark <thc29@MacBook-Pro-2.local>
2018-09-12 10:51:24 +0100

    Removed the default export to avoid the npm warning

--- src/index.js
+++ src/index.js
@@ -21,22 +21,6 @@ import {
   Progress,
 } from './components'
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string,
-  }
-  static defaultProps = {
-    text: 'A default string to show',
-  }
-
-  render() {
-    return (
-      <div>
-        Example Component: {this.props.text}
-      </div>
-    )
-  }
-}
 
 export {
   // uploaderActions,
